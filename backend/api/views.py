f4432eb1dbf1340d860405753793a61ce2745810
Jay Prakash <unijay12@gmail.com>
2021-09-04 22:47:31 +0530

    intial things deleted

--- backend/api/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views.decorators.cache import never_cache
-from django.views.generic import TemplateView
-
-# Create your views here.
-# Serve Vue Application
-index_view = never_cache(TemplateView.as_view(template_name='index.html'))
