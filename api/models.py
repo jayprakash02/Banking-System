60e693aaf4d3645a2cda7d80b4edf86795792828
Jay Prakash <unijay12@gmail.com>
2021-09-04 22:48:05 +0530

    customer and transcation model created

---
+++ api/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+import uuid
+from rest_framework.serializers import ModelSerializer
+# Create your models here.
+class Customer(models.Model):
+    name=models.CharField(max_length=100)
+    cutomer_id=models.UUIDField(default = uuid.uuid4,primary_key=True,auto_created=True)
+    wallet=models.FloatField(default=1000.0)
+    email=models.EmailField()
+
+    def __str__(self):
+        return self.name
+    
+    
+class Transcation(models.Model):
+    transcation_id=models.UUIDField(default=uuid.uuid4,primary_key=True,auto_created=True)
+    date=models.DateTimeField(auto_now=True)
+    to_id=models.ForeignKey(Customer,related_name='to_customer',on_delete=models.CASCADE)
+    from_id=models.ForeignKey(Customer,related_name='from_customer',on_delete=models.CASCADE)
+    amount=models.FloatField(default=1)
+
+    def __str__(self):
+        return 'From:{0} | TO:{1} | AMT:{2}'.format(self.from_id.name,self.to_id.name,self.amount)
+
+class TranscationSerializer(ModelSerializer):
+    class Meta:
+        model = Transcation
+        fields = "__all__"
