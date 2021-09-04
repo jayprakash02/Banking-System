fb8bd002226941735788228e3ebabfa240cd8d03
Jay Prakash <unijay12@gmail.com>
2021-09-05 01:11:57 +0530

    frontend for personal detail section updated

--- src/components/Customer.vue
+++ src/components/Customer.vue
@@ -9,135 +9,38 @@
         <!-- Grid column -->
         <div class="col-xl-5 col-lg-12 mr-0 pb-2">
           <div class="row py-3 pl-4">
-            <div class="col-md-6">
+            <div class="col-md-12">
               <div
                 class="card card-cascade cascading-admin-card dark-card-admin text-white"
               >
                 <!-- Card Data -->
                 <div class="admin-up">
-                  <i
-                    class="far fa-money-bill-alt primary-color mr-3 z-depth-2 text-white"
-                  ></i>
                   <div class="data">
-                    <p class="text-uppercase text-white">sales</p>
-                    <h4 class="font-weight-bold">2000$</h4>
+                    <p class="text-uppercase text-white">Personal Details</p>
+                    <h4 class="font-weight-bold">Jay Prakash</h4>
                   </div>
                 </div>
 
                 <!-- Card content -->
                 <div class="card-body card-body-cascade">
-                  <div class="progress mb-3">
-                    <div
-                      class="progress-bar bg-primary"
-                      role="progressbar"
-                      style="width: 25%"
-                      aria-valuenow="25"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
                   <p class="card-text text-white">
-                    Better than last week (25%)
+                    Email:
                   </p>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div
-                class="card card-cascade cascading-admin-card dark-card-admin text-white"
-              >
-                <!-- Card Data -->
-                <div class="admin-up">
-                  <i
-                    class="far fa-money-bill-alt primary-color mr-3 z-depth-2 text-white"
-                  ></i>
-                  <div class="data">
-                    <p class="text-uppercase text-white">sales</p>
-                    <h4 class="font-weight-bold">2000$</h4>
-                  </div>
-                </div>
+                  <p class="font-weight-bold">unijay12@gmail.com</p>
 
-                <!-- Card content -->
-                <div class="card-body card-body-cascade">
-                  <div class="progress mb-3">
-                    <div
-                      class="progress-bar bg-primary"
-                      role="progressbar"
-                      style="width: 25%"
-                      aria-valuenow="25"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
-                  <p class="card-text text-white">
-                    Better than last week (25%)
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div
-                class="card card-cascade cascading-admin-card dark-card-admin text-white"
-              >
-                <!-- Card Data -->
-                <div class="admin-up">
-                  <i
-                    class="far fa-money-bill-alt primary-color mr-3 z-depth-2 text-white"
-                  ></i>
-                  <div class="data">
-                    <p class="text-uppercase text-white">sales</p>
-                    <h4 class="font-weight-bold">2000$</h4>
-                  </div>
-                </div>
+                  <p class="card-text text-white">Credit Available:</p>
+                  <h4 class="font-weight-bold">2000</h4>
 
-                <!-- Card content -->
-                <div class="card-body card-body-cascade">
                   <div class="progress mb-3">
                     <div
                       class="progress-bar bg-primary"
                       role="progressbar"
-                      style="width: 25%"
-                      aria-valuenow="25"
+                      style="width: 20%"
+                      aria-valuenow="800"
                       aria-valuemin="0"
-                      aria-valuemax="100"
+                      aria-valuemax="1000"
                     ></div>
                   </div>
-                  <p class="card-text text-white">
-                    Better than last week (25%)
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div class="col-md-6">
-              <div
-                class="card card-cascade cascading-admin-card dark-card-admin text-white"
-              >
-                <!-- Card Data -->
-                <div class="admin-up">
-                  <i
-                    class="far fa-money-bill-alt primary-color mr-3 z-depth-2 text-white"
-                  ></i>
-                  <div class="data">
-                    <p class="text-uppercase text-white">sales</p>
-                    <h4 class="font-weight-bold">2000$</h4>
-                  </div>
-                </div>
-
-                <!-- Card content -->
-                <div class="card-body card-body-cascade">
-                  <div class="progress mb-3">
-                    <div
-                      class="progress-bar bg-primary"
-                      role="progressbar"
-                      style="width: 25%"
-                      aria-valuenow="25"
-                      aria-valuemin="0"
-                      aria-valuemax="100"
-                    ></div>
-                  </div>
-                  <p class="card-text text-white">
-                    Better than last week (25%)
-                  </p>
                 </div>
               </div>
             </div>
@@ -150,7 +53,9 @@
             <div
               class="view view-cascade gradient-card-header light-blue lighten-1 white black lighten-4"
             >
-              <h2 class="h2-responsive mb-0 font-weight-500">Sales</h2>
+              <h2 class="h2-responsive mb-0 font-weight-500">
+                Customer Available
+              </h2>
             </div>
 
             <!-- Card content -->
@@ -160,79 +65,124 @@
                 <!-- First column -->
                 <div class="col-md-6">
                   <!-- Date select -->
-                  <p class="lead text-white">
-                    <span class="badge info-color p-2">Data range</span>
-                  </p>
-                  <select
-                    class="mdb-select colorful-select dropdown-info md-form"
+                  <button
+                    type="button"
+                    class="btn btn-primary"
+                    data-toggle="modal"
+                    data-target="#fluidModalRightSuccessDemo"
                   >
-                    <option value="" disabled="" selected=""
-                      >Choose time period</option
+                    Search Customers
+                  </button>
+
+                  <div
+                    class="modal fade right"
+                    id="fluidModalRightSuccessDemo"
+                    tabindex="-1"
+                    role="dialog"
+                    aria-labelledby="myModalLabel"
+                    aria-hidden="true"
+                    data-backdrop="false"
+                  >
+                    <div
+                      class="modal-dialog modal-full-height modal-right modal-notify modal-info"
+                      role="document"
                     >
-                    <option value="1">Today</option>
-                    <option value="2">Yesterday</option>
-                    <option value="3">Last 7 days</option>
-                    <option value="3">Last 30 days</option>
-                    <option value="3">Last week</option>
-                    <option value="3">Last month</option>
-                  </select>
+                      <!-- Content -->
+                      <div class="modal-content">
+                        <!-- Header -->
+                        <div class="modal-header">
+                          <p class="heading lead">Modal Info</p>
+
+                          <button
+                            type="button"
+                            class="close"
+                            data-dismiss="modal"
+                            aria-label="Close"
+                          >
+                            <span aria-hidden="true" class="white-text"
+                              >&times;</span
+                            >
+                          </button>
+                        </div>
+
+                        <!-- Body -->
+                        <div class="modal-body">
+                          <fieldset class="form-check mb-4">
+                            <input
+                              class="form-check-input"
+                              name="group1"
+                              type="radio"
+                              id="radio1"
+                              checked="checked"
+                            />
+                            <label class="form-check-label" for="radio1"
+                              >Option 1</label
+                            >
+                          </fieldset>
+
+                          <fieldset class="form-check mb-4">
+                            <input
+                              class="form-check-input"
+                              name="group1"
+                              type="radio"
+                              id="radio2"
+                            />
+                            <label class="form-check-label" for="radio2"
+                              >Option 2</label
+                            >
+                          </fieldset>
+
+                          <fieldset class="form-check mb-4">
+                            <input
+                              class="form-check-input"
+                              name="group1"
+                              type="radio"
+                              id="radio3"
+                            />
+                            <label class="form-check-label" for="radio3"
+                              >Option 3</label
+                            >
+                          </fieldset>
+                        </div>
+
+                        <!-- Footer -->
+                        <div class="modal-footer justify-content-center">
+                          <a type="button" class="btn btn-info"
+                            >Get it now <i class="far fa-gem ml-1"></i
+                          ></a>
+                          <a
+                            type="button"
+                            class="btn btn-outline-success waves-effect"
+                            data-dismiss="modal"
+                            >No, thanks</a
+                          >
+                        </div>
+                      </div>
+                      <!-- Content -->
+                    </div>
+                  </div>
                 </div>
                 <!-- First column -->
 
                 <!-- Second column -->
                 <div class="col-md-6 text-center pl-xl-2 my-md-0 my-3">
                   <!-- Summary -->
-                  <p class="text-white">
-                    Total sales: <strong>2000$</strong>
-                    <button
-                      type="button"
-                      class="btn btn-info btn-sm p-2 waves-effect waves-light dark-grey-text text-white"
-                      data-toggle="tooltip"
-                      data-placement="top"
-                      title=""
-                      data-original-title="Total sales in the given period"
-                    >
-                      <i class="fas fa-question text-white"></i>
-                    </button>
-                  </p>
-                  <p class="text-white">
-                    Average sales: <strong>100$</strong>
-                    <button
-                      type="button"
-                      class="btn btn-info btn-sm p-2 mr-0 waves-effect waves-light dark-grey-text text-white"
-                      data-toggle="tooltip"
-                      data-placement="top"
-                      title=""
-                      data-original-title="Average daily sales in the given period"
-                    >
-                      <i class="fas fa-question text-white"></i>
-                    </button>
-                  </p>
-
-                  <!-- Change chart -->
-                  <span
-                    class="min-chart my-4"
-                    id="chart-sales"
-                    data-percent="76"
-                    ><span class="percent">76</span
-                    ><canvas height="110" width="110"></canvas
-                  ></span>
-                  <h5>
-                    <span class="badge red accent-2 p-2"
-                      >Change
-                      <i class="fas fa-arrow-circle-up ml-1 text-white"></i
-                    ></span>
-                    <button
-                      type="button"
-                      class="btn btn-info btn-sm p-2 waves-effect waves-light dark-grey-text text-white"
-                      data-toggle="tooltip"
-                      data-placement="top"
-                      title=""
-                      data-original-title="Percentage change compared to the same period in the past"
-                    >
-                      <i class="fas fa-question text-white"></i>
-                    </button>
-                  </h5>
+                  <form action="" method="post"></form>
+                  <div class="md-form md-outline">
+                    <input type="text" id="username12" class="form-control" />
+                    <label for="username12">Amount</label>
+                    <div class="md-form md-outline">
+                      <div class="card-foter text-right">
+                        <button
+                          type="button"
+                          class="btn btn-outline-warning btn-sm waves-effect waves-light"
+                          style="width: 140px;"
+                        >
+                          Send message
+                        </button>
+                      </div>
+                    </div>
+                  </div>
                 </div>
                 <!-- Second column -->
               </div>
